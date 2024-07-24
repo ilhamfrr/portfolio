06500dcb3f87a4621cbe88ad57b4f7c46f0c4e03
ilhamfrr <44331815+ilhamfrr@users.noreply.github.com>
2024-07-25 03:30:19 +0700

    AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -31,9 +31,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Perbaiki penyebabnya, bukan gejalanya!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">IlhamFrr</footer>
         </blockquote>
       </Card.Body>
     </Card>
