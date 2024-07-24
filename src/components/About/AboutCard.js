18cdfd8d49f9f12edc6696dfb605e527011c273b
ilhamfrr <44331815+ilhamfrr@users.noreply.github.com>
2024-07-25 02:36:47 +0700

    AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Ilham Faturahman </span>
+            from <span className="purple"> Jakarta, Indonesia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed a freelance software developer on various freelance platforms such as upwork.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed the Full Stack Developer Class at Codepolitan and Buildwithangga.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
